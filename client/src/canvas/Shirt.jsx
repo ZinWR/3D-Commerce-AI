0012b6f429f9fc548e6801cd85fd7b9c26e553df
QuanN <jeanquan98@gmail.com>
2024-04-05 03:29:54 -0500

    Fixed mesh property typo

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -8,7 +8,7 @@ import path from 'path';
 
 function Shirt() {
     const snap = useSnapshot(state);
-    const { nodes, materials } = useGLTF('./client/public/shirt_baked.glb');
+    const { nodes, materials } = useGLTF('/shirt_baked.glb'); // Assuming the GLTF file is in the public folder
 
     const logoTexture = useTexture(snap.logoDecal);
     const fullTexture = useTexture(snap.fullDecal);
@@ -17,13 +17,11 @@ function Shirt() {
         <group>
             <mesh
                 castShadow
-                geometry={nodes.t_Shirt_male.geometry}
-                material={materials.lambert1}
+                geometry={nodes.T_Shirt_male.geometry} 
+                material={materials.lambert1} 
                 material-roughness={1}
                 dispose={null}
-            >
-                
-            </mesh>
+            />
         </group>
     );
 };
