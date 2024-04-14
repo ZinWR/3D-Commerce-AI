d4dd1476dd9ccaea60cf0fd288f332475af69b3c
QuanN <jeanquan98@gmail.com>
2024-04-13 23:00:33 -0500

    Set up the connection for AI prompt need to fix JSON response

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -48,6 +48,21 @@ const Customizer = () => {
 
     try {
       // call backend to generate AI imgage!
+      setGeneratingImg(true);
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt, 
+        })
+      });
+
+      const data = await response.json();
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`);
+
     } catch (error) {
       alert(error);
     } finally {
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -3,13 +3,33 @@ import * as dotenv from 'dotenv';
 // import { Configuration, OpenAIApi } from 'openai';
 import OpenAI from "openai";
 
-const openai = new OpenAI();
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY
+});
 
 dotenv.config();
 const router = express.Router();
 
 router.route('/').get((req,res) => {
     return res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+})
+
+router.route('/').post(async (req,res) => {
+    try {
+        const { prompt } = req.body;
+        const response = await openai.createImage({
+            prompt,
+            n: 1, 
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+
+        const image = response.data.data[0].b64_json;
+        return res.status(200).json({ photo: image })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 })
 
 export default router;
